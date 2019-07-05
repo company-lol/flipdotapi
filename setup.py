b6bd86c07e593be355f6d5c74af1b58a0884e095
Harper Reed <harper@nata2.org>
2019-07-04 22:15:21 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flipdotapi",
+    version="0.0.1",
+    author="Harper Reed",
+    author_email="harper@company.lol",
+    description="A class that interacts with the flipdot-server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/company-lol/flipdotapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
