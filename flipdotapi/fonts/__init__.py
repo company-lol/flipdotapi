010d68be0c2de1e5ac1359c726ae887111c1d95f
Harper Reed <harper@nata2.org>
2019-07-04 22:10:27 -0500

    added fonts

---
+++ flipdotapi/fonts/__init__.py
@@ -0,0 +1,64 @@
+import os
+from collections import namedtuple
+import fnmatch
+from fontTools import ttLib
+from slugify import slugify
+
+class fonts:
+    
+    FONT_DIRECTORY = os.path.join(os.path.dirname(__file__), './')
+   
+    def __init__(self):
+        pass
+
+    def shortName(self,  font ):
+        FONT_SPECIFIER_NAME_ID = 4
+        FONT_SPECIFIER_FAMILY_ID = 1
+        name = ""
+        family = ""
+        for record in font['name'].names:
+            if record.nameID == FONT_SPECIFIER_NAME_ID and not name:
+                name = record.string.decode('utf-8')
+            elif record.nameID == FONT_SPECIFIER_FAMILY_ID and not family:
+                family = record.string.decode('utf-8')
+            if name and family:
+                break
+        return name, family
+
+    def get_font_files(self, dir_name=None):
+
+        font_files = []
+        font_pattern = '*.ttf'
+
+        if not dir_name:
+            dir_name = self.FONT_DIRECTORY
+        
+        for root, dirs, files in os.walk(dir_name):
+            for filename in fnmatch.filter(files, font_pattern):               
+                tt = ttLib.TTFont(os.path.join(root, filename))
+                name = slugify(self.shortName(tt)[0])
+                font_dir = root.replace(dir_name,"")
+                font_obj = {"name":name, "filename":filename, "path":font_dir}
+                font_files.append(font_obj)
+
+        return font_files
+
+
+    def _font_path(self, path):
+        return os.path.join(self.FONT_DIRECTORY, path)
+
+    def get_fonts(self):
+        TrueTypeFont = namedtuple('TrueTypeFont', ['path', 'points'])
+        fonts = {}
+        font_files = self.get_font_files()
+        for f in font_files:
+            name = f['name']
+            fonts[name] =  TrueTypeFont(
+                self._font_path(f['path'] + "/" + f['filename']), 8)
+
+        return fonts #self.FONTS
+    
+
+
+
+
---
+++ flipdotapi/fonts/fonts/Edge-7x7/readme.txt
@@ -0,0 +1,21 @@
+﻿The font file in this archive was created using Fontstruct the free, online
+font-building tool.
+This font was created by “12Me21”*.
+This font has a homepage where this archive and other versions may be found:
+https://fontstruct.com/fontstructions/show/991997
+
+*NOTE: “Edge 7x7” was originally cloned (copied) from the FontStruction
+“Edge 5x5” (https://fontstruct.com/fontstructions/show/991548) by
+“12Me21”, which is licensed under a Creative Commons Attribution Share Alike
+license (http://creativecommons.org/licenses/by-sa/3.0/).
+
+Try Fontstruct at http://fontstruct.com
+It’s easy and it’s fun.
+
+Fontstruct is copyright ©2014-2018 Rob Meek
+
+LEGAL NOTICE:
+In using this font you must comply with the licensing terms described in the
+file “license.txt” included with this archive.
+If you redistribute the font file in this archive, it must be accompanied by all
+the other files from this archive, including this one.
