9ebfafdf4096a56a2cedc1faeac9c9fb111d4350
Harper Reed <harper@nata2.org>
2019-07-04 23:24:05 -0500

    added numpy to requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flipdotapi",
-    version="0.0.3",
+    version="0.0.4",
     author="Harper Reed",
     author_email="harper@company.lol",
     description="A class that interacts with the flipdot-server",
