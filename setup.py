dd5ba4361f9f7b06ff1d30768eb0b8888182079b
Harper Reed <harper@nata2.org>
2019-07-04 23:26:50 -0500

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flipdotapi",
-    version="0.0.4",
+    version="0.0.5",
     author="Harper Reed",
     author_email="harper@company.lol",
     description="A class that interacts with the flipdot-server",
@@ -14,6 +14,11 @@ setuptools.setup(
     url="https://github.com/company-lol/flipdotapi",
     packages=setuptools.find_packages(),
     include_package_data=True,
+    install_requires=[
+        'Pillow',
+        'fonttools',
+        'numpy',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
